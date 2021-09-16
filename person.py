cc3c1917b67fa500de69581bfcafa0689954e97e
JanPoeppel <82397698+JanPoeppel@users.noreply.github.com>
2021-09-17 00:48:16 +0200

    Update person.py

--- person.py
+++ person.py
@@ -50,6 +50,8 @@ def auth(rfid):
         rfid: Die zu überprüfende RFID
     
     Returns:
+    	'param1': True,
+        'False': pa
         | True: wenn die RFID in der Liste ist.
         | False: wenn die RFID nicht in der Liste ist.
 	"""
