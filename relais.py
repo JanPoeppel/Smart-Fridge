2a90aaca3d62f3c74bb5062ae9d53a01ae3fdc7a
JanPoeppel <82397698+JanPoeppel@users.noreply.github.com>
2021-09-24 13:48:53 +0200

    Create relais.py

---
+++ relais.py
@@ -0,0 +1,14 @@
+import time
+import RPi.GPIO as GPIO
+
+
+def open(timet):
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(19, GPIO.OUT)
+    GPIO.output(19, GPIO.LOW)
+    #relais
+    if(timet > 9):
+        timet = 9
+    #relais
+    time.sleep(timet)
+    GPIO.output(19, GPIO.HIGH)
