b6ea8aa55777064dec9ecdaf70480d1470ae879c
JanPoeppel <82397698+JanPoeppel@users.noreply.github.com>
2021-09-25 16:07:54 +0200

    Update rfid.py

--- rfid.py
+++ rfid.py
@@ -8,6 +8,15 @@ rfid = read()
 """
 
 def read():
+    reader1 = reader.Reader(0x13ba, 0x0018, 10, 8, should_reset=False)
+    reader1.initialize()
+    rfids =str(reader1.read().strip())
+    reader1.disconnect()
+    rfids = rfids.split('\n')[0]
+    return str(rfids)
+    
+    
+    """
     try:
         ret = input("Please scan RFID Chip:\n")
         return ret
@@ -15,3 +24,4 @@ def read():
     except KeyboardInterrupt:
         print('Abbruch')
         return False
+        """
