97fff09052486f7af1f89fd5d5ec6d36fc688ab2
JanPoeppel <82397698+JanPoeppel@users.noreply.github.com>
2021-09-26 00:20:11 +0200

    Update relais.py

--- relais.py
+++ relais.py
@@ -5,10 +5,10 @@ import RPi.GPIO as GPIO
 def open(timet):
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(19, GPIO.OUT)
-    GPIO.output(19, GPIO.LOW)
+    GPIO.output(19, GPIO.HIGH)
     #relais
     if(timet > 9):
         timet = 9
     #relais
     time.sleep(timet)
-    GPIO.output(19, GPIO.HIGH)
+    GPIO.output(19, GPIO.LOW)
