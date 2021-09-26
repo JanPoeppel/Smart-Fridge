af772a33dc6912d7c050bafeba3b285d6b0b3d55
JanPoeppel <82397698+JanPoeppel@users.noreply.github.com>
2021-09-26 14:47:26 +0200

    Update relais.py

--- relais.py
+++ relais.py
@@ -1,8 +1,20 @@
+"""
+Dieses Modul setzt die Werte des GPIO Pins
+"""
 import time
 import RPi.GPIO as GPIO
 
 
 def open(timet):
+    """
+    Aktiviert für die angegebene Zeit den GPIO Pin 19
+    
+    .. note:: Die Zeitspanne wird automatisch auf max. 9s gesetzt. Dies dient zum Schutzfunktion des Magnetschlosses.
+    
+     Args:
+        timet: Die angegebene Zeitspanne
+    
+    """
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(19, GPIO.OUT)
