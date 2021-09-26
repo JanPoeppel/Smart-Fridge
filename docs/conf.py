7fb7b804bc4ff3025320dde3845cce2e60f5c4dc
JanPoeppel <82397698+JanPoeppel@users.noreply.github.com>
2021-09-26 03:53:25 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -12,6 +12,15 @@
 #
 import os
 import sys
+import mock
+
+MOCK_MODULES = [
+    'RPi'
+]
+
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
+
 sys.path.insert(0, os.path.abspath('..'))
 
 
