b25cc77ac10f721d9b99aac358a53371b3d3679a
JanPoeppel <82397698+JanPoeppel@users.noreply.github.com>
2021-09-27 00:37:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 """
 main.py
 
-Dieses Modul kuemmert sich um den Start und das Beenden des Programmes.
-Dabei initalisiert es wichtige Variablen und ueberprueft, ob alle Abhaengigkeiten verfuegbar sind.
+Dieses Modul kümmert sich um den Start und das Beenden des Programmes.
+Dabei initalisiert es wichtige Variablen und überprüft, ob alle Abhängigkeiten verfügbar sind.
 
-Hierfuer muessen die zu pruefenden Module importiert werden.
+Hierfür müssen die zu prüfenden Module importiert werden.
 
 .. note:: Die exit() Funktion wird auch bei einem KeyboardInterrupt Event aufgerufen.
 
@@ -25,44 +25,44 @@ LOGPATH = None
 
 def main():
     """
-	Starten und Beenden des Programmes
-	"""
+    Starten und Beenden des Programmes
+    """
     start()
     exit()
 
 def start():
-	"""
-	Starten des Programmes:
-	
-	Initialisiert das Einstellungsmodul :class:`settings.py` und setzt den LOGPATH auf den in der Datei hinterlegten Pfad.
-	Danach wird die GUI gestartet.
-	"""
-	try:
-		#init all modules
-		settings.init()
-		LOGPATH = str(settings.getPath('log.log'))
-		logging.basicConfig(filename='log.log',level=logging.INFO)
-		
-		person.init()
-		
-		gui.start()		
-		
-		
-	except SystemExit:
-		exit()		
-	except KeyboardInterrupt:
-		exit()
-	
+    """
+    Starten des Programmes:
+    
+    Initialisiert das Einstellungsmodul :class:`settings.py` und setzt den LOGPATH auf den in der Datei hinterlegten Pfad.
+    Danach wird die GUI gestartet.
+    """
+    try:
+        #init all modules
+        settings.init()
+        LOGPATH = str(settings.getPath('log.log'))
+        logging.basicConfig(filename='log.log',level=logging.INFO)
+        
+        person.init()
+        
+        gui.start()        
+        
+        
+    except SystemExit:
+        exit()        
+    except KeyboardInterrupt:
+        exit()
+    
 def exit():
-	"""
-	Beenden des Programmes:
-	
-	Diese Funktion wird als letztes aufgerufen und kann daher zum Aufraeumen genutzt werden.
-	Zum Beispiel koennen noch gesetzte GPIOs zurueckgesetzt werden.
-	"""
-	#GPIO.cleanup()
-	logging.warning('Exiting Script')
-	print('Exit')
+    """
+    Beenden des Programmes:
+    
+    Diese Funktion wird als letztes aufgerufen und kann daher zum Aufräumen genutzt werden.
+    Zum Beispiel können noch gesetzte GPIOs zurückgesetzt werden.
+    """
+    #GPIO.cleanup()
+    logging.warning('Exiting Script')
+    print('Exit')
 
 
 
