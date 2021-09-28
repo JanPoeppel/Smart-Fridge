415b1ad09cfb8143b01469b1c976dd84ccb581bc
JanPoeppel <82397698+JanPoeppel@users.noreply.github.com>
2021-09-28 03:29:21 +0200

    Create node.py

---
+++ node.py
@@ -0,0 +1,30 @@
+"""
+node.py
+
+Dieses Modul kümmert sich um das Senden von Nachrichten an NodeRED
+
+Typisches Anwendungsbeispiel:
+
+"""
+ENABLED=False
+try:
+    import requests
+    ENABLED = True
+except ImportError:
+    ENABLED=False
+
+
+def sendMessage(msg):
+    """
+    Sendet eine Nachricht an die Node-RED Schnittstelle
+
+    Args:
+        msg: Die zu sendende Nachricht
+    """
+    if not (ENABLED):
+        return
+    headers = {'Content-type': 'text/plain'}
+    url = 'http://localhost:1880/hello-raw'
+    res = requests.post(url, headers=headers, data = msg)
+    print(res.text)
+    
