d1bdd378111542b357dbf322229e57d4c3f6e5a0
JanPoeppel <82397698+JanPoeppel@users.noreply.github.com>
2021-09-28 04:27:48 +0200

    up

--- money.py
+++ money.py
@@ -49,7 +49,7 @@ def withdraw(rfid, amount):
         False -- Wenn ein Fehler aufgetreten ist.
     """
     rfid = str(rfid)
-    return addMoney(rfid, -(int(amount))
+    return addMoney(rfid, -(int(amount)))
 
 def getMoney(rfid):
     """
